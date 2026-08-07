02a0898bfc75af903dcfc3ace898c2917f01bb88
anantiku-21 <anantbaraik@gmail.com>
2026-08-08 02:08:54 +0530

    Add files via upload

---
+++ videos_to_mp3.py
@@ -0,0 +1,11 @@
+# Converts the videos to mp3 
+import os 
+import subprocess
+
+files = os.listdir("videos") 
+print(files)
+for file in files: 
+    tutorial_number = file.split(". ")[0]
+    file_name = file.split(". ")[1].split(".m")[0]
+    print( tutorial_number,  file_name)
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
